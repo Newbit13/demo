f8900fb7d969146945ca8dba52dceb438d34e043
Newbit13 <525842854@qq.com>
2020-10-20 18:01:38 +0800

    feat：题库

--- react/ice_test/romantic/romantic_admin/src/pages/SubjectCreate/components/EditForm/index.tsx
+++ react/ice_test/romantic/romantic_admin/src/pages/SubjectCreate/components/EditForm/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState,JSX } from 'react';
-import { Input, Form, Box, Button, Card, DatePicker, Message, Radio, Upload, Divider } from '@alifd/next';
-import { useSetState } from 'ahooks';
+import { Input, Form, Box, Button, Card, DatePicker, Message, Radio, Upload, Divider, Drawer, Checkbox, NumberPicker } from '@alifd/next';
 
 import { UploadProps } from '@alifd/next/types/upload';
 import { Moment } from 'moment';
@@ -25,7 +24,6 @@ export interface DataSource {
 export interface BasicFormProps {
   dataSource?: DataSource;
   onSubmit?: () => void;
-  onCancel?: () => void;
 }
 
 export interface subjcetSchma {
@@ -40,61 +38,89 @@ export interface subjcetSchma {
   }[];
 }
 
-const DEFAULT_DATA: DataSource = {
-  type: 'private',
-};
-
-const DEFAULT_ON_SUBMIT = (values: BasicFormProps, errors: []): void => {
-  if (errors) {
-    console.log('errors', errors);
-    return;
-  }
-  console.log('values:', values);
-  Message.success('提交成功');
-};
-
 const BasicForm: React.SFC<BasicFormProps> = (props): JSX.Element => {
-  const {
-    dataSource = DEFAULT_DATA,
-    onSubmit = DEFAULT_ON_SUBMIT,
-    onCancel = () => { },
-  } = props;
-
-  const [postData, setValue] = useState<BasicFormProps>(dataSource);
+  const [postData, setValue] = useState<BasicFormProps>({
+    canSkip:true,
+    aspect:[],
+    type:'single',
+    title:'',
+    item:[]
+  });
   const [subjcetSchma,setSubjcetSchma] = useState<subjcetSchma[]>([]);
-
+  const [editBoxVisible,setEditBoxVisible] = useState(false);
+  console.log(postData);
+  
   const formChange = (values: BasicFormProps): void => {
-    console.log({values});
-    setValue(values);
+    setValue({
+      canSkip:values['skip'],
+      aspect:[],
+      type:values['type'],
+      title:values['subjectItemTitle'],
+      item:postData.item.map((v,i)=>({
+        key:v.key,
+        text:values['subjectItem'+(i+1)],
+        value:values['subjectItemValue'+(i+1)]
+      }))
+    });
   };
 
-  const sigleButton = ()=>{
-    const itemIndex = subjcetSchma.length;
+  const onSubmit = (values: BasicFormProps, errors: []): void => {
+    if (errors) {
+      console.log('errors', errors);
+      return;
+    }
+    console.log('values:', values);
     const newValue = [
       ...subjcetSchma,
       {
-        index:itemIndex,
-        canSkip:true,
-        aspect:[],
-        type:'single',
-        title:'',
-        item:[{
-          text:'',
-          value:0
-        }]
+        ...postData
       }
     ];
-    // console.log(newValue);
-    console.log(typeof newValue);
     setSubjcetSchma(newValue)
-  }
+    Message.success('提交成功');
+    onClose();
+  };
 
-  const doubleButton = ()=>{
-
+  const addSubject = ()=>{
+    setEditBoxVisible(true);
+    setValue({
+      canSkip:true,
+      aspect:[],
+      type:'single',
+      title:'',
+      item:[]
+    });
   }
 
   const addItem = ()=>{
-    
+    setValue({
+      ...postData,
+      item:[
+        ...postData.item,
+        {
+          key: Date.now(),
+          text:'',
+          value:0
+        }
+      ]
+    })
+  }
+
+  const delItem = (i)=>()=>{
+    const newSubjectItem = [
+      ...postData.item
+    ];
+    newSubjectItem.splice(i,1)
+    setValue({
+      ...postData,
+      item:[
+        ...newSubjectItem
+      ]
+    })
+  }
+
+  const onClose = ()=>{
+    setEditBoxVisible(false);
   }
 
   return (
@@ -102,58 +128,73 @@ const BasicForm: React.SFC<BasicFormProps> = (props): JSX.Element => {
       <Card.Content>
         <Box spacing={20} direction="row">
           <div>
-            <Button onClick={sigleButton} type="primary" style={{marginBottom:'10px'}}>单选题</Button>
-            <br />
-            <Button onClick={doubleButton} type="primary">多选题</Button>
+            <div className="preview">
+              {subjcetSchma.map((v,vi)=>(
+                <div key={vi}>
+                  <h1>{vi + 1}、 {v.canSkip?'':<span style={{color:'red'}}>*</span>}{v.title}<span style={{color:'red'}}>【{v.type=='single'?'单选':(v.type=='double'?'多选':'填空')}】</span></h1>
+                  {v.item.map((v1,vindex1)=>(
+                    <p key={vindex1}>    {vindex1 + 1}、 {v1.text}</p>
+                  ))}
+                </div>
+              ))}
+            </div>
+            <Button onClick={addSubject} type="primary" style={{marginBottom:'10px'}}>添加题目</Button>
           </div>
-          {
-            subjcetSchma.map((v)=>(
-              <Form
-                className={styles.BasicForm}
-                responsive
-                fullWidth
-                // value={postData}
-                labelAlign="top"
-                // onChange={formChange}
-                key={v.index}
-              >
-                <FormItem {...formItemLayout} label="题目名称：">
-                  <Input placeholder="请输入题目名称" name="subjectItemTitle" />
+          <Drawer
+            title="编辑题目"
+            width={480}
+            placement="right"
+            visible={editBoxVisible}
+            onClose={onClose}>
+            <Form
+              className={styles.BasicForm}
+              responsive
+              fullWidth
+              value={postData}
+              labelAlign="top"
+              onChange={formChange}
+            >
+              <FormItem {...formItemLayout} label="题目名称：" required requiredMessage="必填" defaultValue={postData.title}>
+                <Input placeholder="请输入题目名称" name="subjectItemTitle" />
+              </FormItem>
+              <FormItem {...formItemLayout} label="题目类型：" >
+              <Radio.Group name="type"defaultValue={postData.type} >
+                  <Radio id="single" value="single">单选</Radio>
+                  <Radio id="double" value="double">多选</Radio>
+                  <Radio id="double" value="freetxt">填空</Radio>
+                </Radio.Group>
+              </FormItem>
+              <FormItem {...formItemLayout} label="其他:">
+                    <Checkbox name="skip" checked={postData.canSkip}>可跳过</Checkbox>
+              </FormItem>
+              {/* <Divider dashed /> */}
+              {postData.item.map((v,i)=>(
+                <FormItem {...formItemLayout} label={'选项'+(i + 1)+'：'} key={v.key}>
+                  <Input placeholder="请输入选项" name={"subjectItem"+(i+1)} defaultValue={v.text}/>
+                  <NumberPicker  placeholder="请输入分值" name={"subjectItemValue"+(i+1)} defaultValue={v.value}/>
+                  <Button onClick={delItem(i)} type="primary">删除</Button>
                 </FormItem>
-                <Divider dashed />
-                <FormItem {...formItemLayout} label="选项：">
-                  <Input placeholder="请输入选项" name="subjectItem" />
-                </FormItem>
-                <FormItem colSpan={12}>
-                  <Button onClick={addItem} type="primary" style={{marginBottom:'10px'}}>增加选项</Button>
-                </FormItem>
-              </Form>
-            ))
-          }
-            
-            
-            {/* <FormItem {...formItemLayout} label="项目权限：" >
-              <Radio.Group name="type" aria-labelledby="authority of project">
-                <Radio id="private" value="private">私密项目</Radio>
-                <Radio id="internal" value="internal">内部项目</Radio>
-                <Radio id="publish" value="publish">开放目</Radio>
-              </Radio.Group>
-            </FormItem>
+              ))}
+              
+              <FormItem colSpan={12}>
+                <Button onClick={addItem} type="primary">增加选项</Button>
+              </FormItem>
+              <FormItem colSpan={12}>
+                <Box spacing={8} direction="row">
+                  <Form.Submit
+                    type="primary"
+                    onClick={onSubmit}
+                    validate
+                  >确定</Form.Submit>
+                </Box>
+              </FormItem>
+            </Form>
+          </Drawer>
+            {/*
 
             <FormItem {...formItemLayout} label="项目描述：" >
               <Input.TextArea placeholder="请输入项目详细信息" name="desc" />
             </FormItem> */}
-
-            <FormItem colSpan={12}>
-              <Box spacing={8} direction="row">
-                <Form.Submit
-                  type="primary"
-                  onClick={onSubmit}
-                  validate
-                >确定</Form.Submit>
-                <Button onClick={onCancel} type="secondary">取消</Button>
-              </Box>
-            </FormItem>
         </Box>
       </Card.Content>
     </Card>
