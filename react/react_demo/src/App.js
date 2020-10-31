466db00cb46f1c2e3c96da1d2c87209b0df2d96e
Newbit13 <525842854@qq.com>
2020-10-31 17:59:27 +0800

    feat：react动态加载

--- react/react_demo/src/App.js
+++ react/react_demo/src/App.js
@@ -1,9 +1,14 @@
-import React,{useState,useEffect} from 'react';
+import React,{useState,useEffect,lazy,Suspense} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Foo = lazy(() => import('./Foo'));
+
+
 function App() {
   const [mylist,setMyList] = useState([]);
+  const [Cmp,upCmp] = useState(null);
+  const [test,upTest] = useState("gulugulu");
   useEffect(()=>{
     console.log(111);
     const temp = [];
@@ -14,6 +19,26 @@ function App() {
     }
     setMyList(temp);
   },[]);
+
+  function loadCmp(){
+    //用import才能在需要组件时异步加载相应的js
+    // const Foo = require('./Foo');
+    // upCmp(Foo);
+
+    // webpack提供了import供异步执行和独立出来打包成ChunkName,react.lazy原理与此相同
+    const Foo = import(/* webpackChunkName: 'Foo' */'./Foo');
+    Foo.then((v)=>{
+      console.log(v.default);
+      upCmp(v);
+      upTest(function(...rest){
+        console.log(rest);
+      });
+      // upCmp(v.default);//之后Cmp不是v.default的值，而是v.default执行后的值
+    })
+  }
+
+  // console.log(Cmp);
+  
   return (
     <div className="App">
       <button onClick={()=>{
@@ -30,6 +55,11 @@ function App() {
           <input type="text" />
         </div>
       ))}
+      <button onClick={loadCmp}>动态加载组件</button>
+      {Cmp?React.createElement(Cmp.default):null}
+      <Suspense fallback={<div>loading...</div>}>
+        <Foo />
+      </Suspense>
     </div>
   );
 }
---
+++ react/react_demo/src/Foo.js
@@ -0,0 +1,7 @@
+import React,{useState,useEffect} from 'react';
+
+function Foo(){
+    return <h1>FFFFXXXK</h1>
+}
+
+export default Foo;
