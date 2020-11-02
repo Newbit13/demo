580c3d65d8b52af3832c08dc5e2056c07e67d227
Newbit13 <525842854@qq.com>
2020-11-03 01:52:07 +0800

    feat：小数精度问题

--- node/util/number_util/number_util.js
+++ node/util/number_util/number_util.js
@@ -23,8 +23,30 @@ function getCalculatorDecimalDigits(a){
 // console.log(1.000000000000001 * 10000000000000000);
 // console.log(1.000000000000001 * 1000000000000000);
 
-var Decimal = require('decimal.js');
-x = new Decimal(1.0000000000000001) //1
-y = new Decimal(1.0000000000000001) 
-z = x.plus(y)
-console.log(z);//2
+// var Decimal = require('decimal.js');
+// x = new Decimal(1.0000000000000001) //1
+// y = new Decimal(1.0000000000000001) 
+// z = x.plus(y)
+// console.log(z);//2
+
+let a = '';
+for(var i=0;i<53;i++)a+=1;
+
+// let b = '';
+// for(var i=0;i<52;i++)b+=0;
+// b += 1;
+
+let result = parseInt(a,2)*Math.pow(2,-52)*Math.pow(2,1023);
+let result2 = Math.pow(2,-52)*Math.pow(2,-1022);//parseInt(b,2)*Math.pow(2,-52)*Math.pow(2,1023);
+
+let gapValue = Math.pow(2,-52)*Math.pow(2,1023);//2^971
+
+console.log(result);
+console.log(result == Number.MAX_VALUE);
+
+console.log(result2);
+console.log(result - result2);
+console.log(gapValue);
+console.log(gapValue - 1);
+console.log(Math.pow(2,971));
+console.log(Math.pow(2,971) - 1);
