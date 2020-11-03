5526daee7f4428f717aa873ce0872541cce3cea7
Newbit13 <525842854@qq.com>
2020-11-03 18:11:14 +0800

    feat：class继承的原理

--- js/interview/README.md
+++ js/interview/README.md
@@ -1,23 +1,7 @@
 # 写一个 es6 的继承过程
-// 这个是要实现的方法
-createClass = fun(sons, super) {
-	// TODO
-	return fn;
-}
 
-// 这是个 es6 的一个例子，要实现 extends 的功能。
-class Man extends Human {
-	cons (args) {
-		super(args)
-	  // xxxxx
-	}
 
-	speak() {
-		console.log('')
-	}
-}
-
-#写一个大数相乘的解决方案。传两个字符串进来，返回一个字符串
+# 写一个大数相乘的解决方案。传两个字符串进来，返回一个字符串
 function multi(str1, str2) {
 
 }
--- js/interview/index.js
+++ js/interview/index.js
@@ -1,11 +1,33 @@
 // 写一个 es6 的继承过程,要实现 extends 的功能。
-const createClass = function(sons, _super) {
+const createExtends = function(sons, ssuper) {
 	// TODO
-	return {
-        extends:function(){
-
-        }
-    };
+    sons.__proto__ = ssuper;//继承静态属性
+    sons.prototype.__proto__ = ssuper.prototype;//继承动态属性
 }
 
-createClass()
+class Human{
+  static ha = 1
+  static la = 2
+  speak() {
+    console.log('i am human')
+  }
+}
+
+class Man{
+  static zz = 3
+  static la = 5
+  // speak() {
+  //   console.log('i am man')
+  // }
+}
+
+createExtends(Man,Human)
+
+console.log(Human.la);
+console.log(Human.ha);
+
+console.log(Man.la);
+console.log(Man.ha);
+
+console.log(Man.speak);
+console.log(new Man().speak());
