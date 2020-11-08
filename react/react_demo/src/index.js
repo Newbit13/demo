c273915de4db10b6353cb9811002fbf3a98f477c
Newbit13 <525842854@qq.com>
2020-11-09 00:39:15 +0800

    feat：react class组件跟function组件的区别

--- react/react_demo/src/index.js
+++ react/react_demo/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';//异步加载
+import FnVsClass from './FnVsClass'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <FnVsClass />
   </React.StrictMode>,
   document.getElementById('root')
 );
