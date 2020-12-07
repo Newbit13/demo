4d10516cd842eff2eb258fdb46c56c3c29f081c4
Newbit13 <525842854@qq.com>
2020-12-07 20:50:53 +0800

    feat：ast demo

--- ast/src/testEditCode.js
+++ ast/src/testEditCode.js
@@ -3,9 +3,14 @@ const traverse = require("babel-traverse").default;
 const t = require('babel-types');
 const generate = require('babel-generator').default;
 
-const code = `function square(n) {
+const code = `
+function square(n) {
   return n * n;
-}`;
+}
+function add() {
+    return 1 + 2;
+}
+`;
 
 let ast = babylon.parse(code,{
     // sourceType: "module", // default: "script"
@@ -21,23 +26,45 @@ traverse(ast,{
         
         path.node.left = t.identifier("sebmck");
         path.replaceWith(
-            // t.binaryExpression("**", path.node.left, t.identifier("newbit"))
             t.binaryExpression("**", path.node.left, t.numericLiteral(2))
         );
     },
-    FunctionDeclaration(path) {
-        path.insertBefore(
-            t.expressionStatement(
-                t.callExpression(
-                    t.memberExpression(
-                        t.identifier("console"),
-                        t.identifier("log")
-                    ),
-                    [t.stringLiteral(path.node.id.name + " is be call")]
-                )
-            )
-        );
-    }
+    FunctionDeclaration(path) { 
+        path.traverse({
+            Identifier(path){
+                if(path.isIdentifier({name:'add'})){
+                    let functionPath = this.functionPath;
+                    functionPath.traverse({
+                        BlockStatement(path){
+                            path.node.body.unshift(
+                                t.expressionStatement(
+                                    t.callExpression(
+                                        t.memberExpression(
+                                            t.identifier("console"),
+                                            t.identifier("log")
+                                        ),
+                                        [t.stringLiteral(functionPath.node.id.name + " is be called")]
+                                    )
+                                )
+                            );
+                            // insetBlockStart
+                        }
+                    })
+                    functionPath.insertBefore(
+                        t.expressionStatement(
+                            t.callExpression(
+                                t.memberExpression(
+                                    t.identifier("console"),
+                                    t.identifier("log")
+                                ),
+                                [t.stringLiteral(functionPath.node.id.name + " is be definded")]
+                            )
+                        )
+                    );
+                }
+            }   
+        },{functionPath:path})
+    },
 });
 
 //根据ast生成代码
