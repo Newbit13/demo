18eac42dc23b48cd7028e02cee91b5392eca9976
Newbit13 <525842854@qq.com>
2020-12-14 21:29:41 +0800

    feat：学习commander.js的使用

--- cli/src/index.js
+++ cli/src/index.js
@@ -1,29 +1,38 @@
 const chalk = require('chalk');
-const { program } = require('commander');
+const program = require('commander');
 
 program.version('0.0.1');//--version
 program.name("cc").usage('<command> [options]');
 
-program
-    .option('-d, --debug', 'output extra debugging')
-    .option('-s, --small <type>', 'small pizza size',123)
-    .option('-p, --pizza-type <type>', 'flavour of pizza','fffloat');
-    // .option('-p, --pizza-type <type>', 'flavour of pizza','fffloat')//执行时不用-p、--pizza-type、参数也不用
+
 
 program
-    .command('init <aaa> [bbbb]', 'Update packages of taro')
-    .option('-sb, --sbbbb', 'hhhhhhhh')
+    .command('build')
+    .arguments('<cmd> [env]')
+    .action((cmd,env)=>{
+        console.log(cmd);
+        console.log(env);
+    });
+
+program
+    // .command('init <aaa> [bbbb]', 'init packages of cc')
+    .command('init <aaa> [bbbb]')//没有第二个参数时action才会执行
+    .description('init~~~~~')
     .action((aaa,bbb)=>{
         console.log(aaa);
         console.log(bbb);
-    });
+    })
+    .option('-sb, --sbbbb', 'hhhhhhhh')
+    .option('-s, --small <type>', 'small pizza size',123)//<必选> [可选]
+    .option('-p, --pizza <type>', 'flavour of pizza');
+    
 
 
 program.parse(process.argv);//process.argv 可以拿到在命令行的输入
 
-//有默认值或者有提到其参数的，这里会为true
-if (program.debug){
-    console.log(chalk.blue('debug'));
+//提到option的，program.xx为true；有设置值或者没提到但有默认值的，program.xx为该值
+if (program.sbbbb){
+    console.log(chalk.blue('sbbbb'));
 }
 
 if (program.small){
@@ -33,5 +42,6 @@ if (program.pizzaType){
     console.log(chalk.blue('pizzaType'));
 }
 
-console.log(chalk.blue(program.pizzaType));
-console.log(chalk.blue(program.small));
+// console.log(chalk.blue(program.pizzaType));
+// console.log(chalk.blue(program.small));
+// console.log(chalk.blue(program.sbbbb));
