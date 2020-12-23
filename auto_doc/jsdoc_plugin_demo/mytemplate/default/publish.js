df67f4ff99cad7d56077af46f1c2f7fae7310cd1
Newbit13 <525842854@qq.com>
2020-12-23 18:45:14 +0800

    feat：自定义模板

---
+++ auto_doc/jsdoc_plugin_demo/mytemplate/default/publish.js
@@ -0,0 +1,12 @@
+/**
+ * Generate documentation output.
+ *
+ * @param {TAFFY} data - A TaffyDB collection representing
+ *                       all the symbols documented in your code.
+ * @param {object} opts - An object with options information.
+ * @param {Tutorial} tutorials
+ */
+exports.publish = function(taffyData, opts, tutorials) {
+    // 我们需要的数据都在 taffyData 中
+    console.log(taffyData);
+}
