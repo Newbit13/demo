35c8863da04dff1051ce0d86232dc56b58a17581
Newbit13 <525842854@qq.com>
2021-02-10 00:22:33 +0800

    feat：karma demo

---
+++ tools/karma_test_demo/README.md
@@ -0,0 +1,3 @@
+如果安装了webpack，却提示找不到，那很有可能是路径问题。
+首先卸载所有karama、karama-cli：```npm uninstall karama karama-cli```
+在重新安装：注意karama-cli要-g全局安装，karama需要-D安装
--- tools/karma_test_demo/test/index.js
+++ tools/karma_test_demo/test/index.js
@@ -1,6 +1,6 @@
-import {a} from 'src/index.js'
+import {a} from '../src/index.js'
 // var a =1;
-
+// console.log(a);
 describe('haha', function () {
     it('should xxxx', function () {
         expect(a).toBe(1)
