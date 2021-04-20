d846d8ad0a433590171804f31c918b0d8583914d
Newbit13 <525842854@qq.com>
2021-04-21 01:35:34 +0800

    no message

--- react_native_demo/MyTestApp/src/screen/DIY/index.js
+++ react_native_demo/MyTestApp/src/screen/DIY/index.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect,useCallback,useRef} from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import {
     StyleSheet,
     View,
@@ -7,8 +7,10 @@ import {
     Image,
     ImageBackground,
     ScrollView,
-    NativeModules
+    NativeModules,
 } from 'react-native';
+
+import { WebView } from 'react-native-webview';
 
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
@@ -17,15 +19,15 @@ import LinearGradinet from 'react-native-linear-gradient';
 
 
 const styles = StyleSheet.create({
-    scroll:{
+    scroll: {
         // flex:1,
         // height:100,
         backgroundColor: '#24a5ff',
     },
-    normalBtn:{
-        width:100,
-        borderRadius:20,
-        overflow:'hidden',
+    normalBtn: {
+        width: 100,
+        borderRadius: 20,
+        overflow: 'hidden',
         // backgroundColor: '#e0708c'
     },
 });
@@ -36,23 +38,26 @@ class HomeScreen extends React.Component {
     };
     render() {
         return (
-        <ScrollView style={styles.scroll}>
-            <Text>Android原生模块!</Text>
-            <View style={styles.normalBtn} backgroundColor='#ffff00'>
-                <Button
-                    title="触发功能"
-                    // onPress={() => console.log(NativeModules.ToastExample)}
-                    onPress={() => {
-                        // console.log(typeof NativeModules.ToastExample.show);
-                        NativeModules.ToastExample.show("我是通过原生模块调用的toast", NativeModules.ToastExample.LONG);
-                        NativeModules.ToastExample.testCallBack(function(v){
-                            console.log('这是回调的：',v);
-                        });
-                        console.log(1);
-                    }}
-                />
-            </View>
-        </ScrollView>
+            // <WebView source={{ uri: 'https://www.icardfinancial.com/kaduofen_ankangwuyou/index.html#/member/rightsHome?orderId=1192104100013156481&encrypt=kNfH%2Byv8UeAPDXl35BO7slAEyWBBMUezu8LW2wNzsjzTnBo%2BCZBvi4TvAwW46yKewoG1TkPsdLt88vliKnUhIiIflFHLKaTd9AaX4KlOUe0%2BdOBWs3YnVlRlzaXBVDa67l4b6ufRaK2SoKyuBCf22cxc20nYGEVTtHHidtFElnA%3D&qd=baijinxinyong-sdk' }} />
+            <WebView source={{ uri: 'https://url.cn/7wILQFbB' }} />
+            // <ScrollView style={styles.scroll}>
+            //     <Text>Android原生模块!</Text>
+            //     <View style={styles.normalBtn} backgroundColor='#ffff00'>
+            //         <Button
+            //             title="触发功能"
+            //             // onPress={() => console.log(NativeModules.ToastExample)}
+            //             onPress={() => {
+            //                 // console.log(typeof NativeModules.ToastExample.show);
+            //                 NativeModules.ToastExample.show("我是通过原生模块调用的toast", NativeModules.ToastExample.LONG);
+            //                 NativeModules.ToastExample.testCallBack(function (v) {
+            //                     console.log('这是回调的：', v);
+            //                 });
+            //                 console.log(1);
+            //             }}
+            //         />
+            //     </View>
+                
+            // </ScrollView>
         );
     }
 }
