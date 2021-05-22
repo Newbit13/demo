5b765d591bea74e9ac0f0c55c181c03c9b729111
Newbit13 <525842854@qq.com>
2021-05-22 11:27:15 +0800

    no message

--- flutter_demo/demo/flutter_application_1/lib/MyHomePage2.dart
+++ flutter_demo/demo/flutter_application_1/lib/MyHomePage2.dart
@@ -22,58 +22,58 @@ class _MyHomePage2State extends State<MyHomePage2> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // Container(
-            //   width: 100,
-            //   height: 100,
-            //   color: Colors.red,
-            //   child: Center(
-            //     child: Text(
-            //       "1",
-            //       style: TextStyle(color: Colors.white, fontSize: 38),
-            //     ),
-            //   ),
-            // ),
-            // Container(
-            //   width: 100,
-            //   height: 100,
-            //   color: Colors.pink,
-            //   child: Center(
-            //     child: Text(
-            //       "2",
-            //       style: TextStyle(color: Colors.white, fontSize: 38),
-            //     ),
-            //   ),
-            // ),
-            // Container(
-            //   width: 100,
-            //   height: 100,
-            //   color: Colors.blue,
-            //   child: Center(
-            //     child: Text(
-            //       "3",
-            //       style: TextStyle(color: Colors.white, fontSize: 38),
-            //     ),
-            //   ),
-            // ),
-            // SizedBox(
-            //   height: 10,
-            // ),
-            // Row(
-            //   children: [
-            //     Expanded(
-            //         flex: 1,
-            //         child: Container(
-            //           height: 30,
-            //           color: Colors.blue,
-            //         )),
-            //     Expanded(
-            //         flex: 1,
-            //         child: Container(
-            //           height: 30,
-            //           color: Colors.red,
-            //         ))
-            //   ],
-            // ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+              child: Center(
+                child: Text(
+                  "1",
+                  style: TextStyle(color: Colors.white, fontSize: 38),
+                ),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.pink,
+              child: Center(
+                child: Text(
+                  "2",
+                  style: TextStyle(color: Colors.white, fontSize: 38),
+                ),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              child: Center(
+                child: Text(
+                  "3",
+                  style: TextStyle(color: Colors.white, fontSize: 38),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: Container(
+                      decoration: BoxDecoration(color: Colors.amber),
+                      color: Colors.blue,
+                    )),
+                Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: 30,
+                      color: Colors.red,
+                    ))
+              ],
+            ),
             D(
               color: Colors.blue,
             ),
---
+++ flutter_demo/demo/flutter_application_1/lib/Page1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Page extends StatelessWidget {
+  const Page({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      width: 300,
+      color: Colors.blue,
+    );
+  }
+}
