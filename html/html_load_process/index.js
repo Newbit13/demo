7277f3c69e0aa1587c6839ec21c4c04db475142c
Newbit13 <525842854@qq.com>
2021-05-26 16:12:31 +0800

    feat：1px问题

--- html/html_load_process/index.js
+++ html/html_load_process/index.js
@@ -1,5 +1,12 @@
 console.log(document.readyState);
 
+// iframe的缺点
+// 1.iframe会阻塞主页面的Onload事件
+// 2.搜索引擎的检索程序无法解读这种页面，不利于SEO
+// 3.iframe和主页面共享连接池，而浏览器对相同域的连接有限制，所以会影响页面的并行加载
+// 通过动态赋予iframe src的值，可以解决1、3问题
+document.getElementById("haha").src = "https://www.baidu.com"
+
 setTimeout(()=>{
     console.log(document.readyState);
 },1000);
