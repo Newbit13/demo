b8b55d26be24eeabe10d386a815bf0a4771315ba
Newbit13 <525842854@qq.com>
2021-07-29 17:56:47 +0800

    feat：识别猫

--- ml/partice/course1-week2/lr_utils.py
+++ ml/partice/course1-week2/lr_utils.py
@@ -4,7 +4,6 @@ import h5py
     
 def load_dataset():
     train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
-    # print(type(train_dataset["train_set_y"][:])) #<class 'numpy.ndarray'>
     train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
     train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
 
