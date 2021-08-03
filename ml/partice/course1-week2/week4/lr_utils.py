dd86e29d8f2e8be281d624c49d8e04877279d838
Newbit13 <525842854@qq.com>
2021-08-03 22:38:28 +0800

    no message

--- ml/partice/course1-week2/week4/lr_utils.py
+++ ml/partice/course1-week2/week4/lr_utils.py
@@ -12,12 +12,12 @@ def load_dataset():
     test_set_x_orig = np.array(test_dataset["test_set_x"]) # your test set features
     test_set_y_orig = np.array(test_dataset["test_set_y"]) # your test set labels
 
-    # classes = np.array(test_dataset["list_classes"]) # the list of classes
+    classes = np.array(test_dataset["list_classes"]) # the list of classes
     
     train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
     test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
     
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig#, classes
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
 def sigmoid(z):
     """
@@ -28,7 +28,28 @@ def sigmoid(z):
         s  -  sigmoid（z）
     """
     s = 1 / (1 + np.exp(-z))
-    return s
+    return s,z
+
+def sigmoid_backward(dA, cache):
+    """
+    Implement the backward propagation for a single SIGMOID unit.
+
+    Arguments:
+    dA -- post-activation gradient, of any shape
+    cache -- 'Z' where we store for computing backward propagation efficiently
+
+    Returns:
+    dZ -- Gradient of the cost with respect to Z
+    """
+
+    Z = cache
+
+    s = 1/(1+np.exp(-Z))
+    dZ = dA * s * (1-s)
+
+    assert (dZ.shape == Z.shape)
+
+    return dZ
 
 def relu(z):
     """
@@ -38,5 +59,27 @@ def relu(z):
     返回：
         s  -  relu
     """
-    s = max(0,z)
-    return s
+    s = np.maximum(0,z)
+    return s,z
+
+def relu_backward(dA, cache):
+    """
+    Implement the backward propagation for a single RELU unit.
+
+    Arguments:
+    dA -- post-activation gradient, of any shape
+    cache -- 'Z' where we store for computing backward propagation efficiently
+
+    Returns:
+    dZ -- Gradient of the cost with respect to Z
+    """
+
+    Z = cache
+    dZ = np.array(dA, copy=True) # just converting dz to a correct object.
+
+    # When z <= 0, you should set dz to 0 as well. 
+    dZ[Z <= 0] = 0
+
+    assert (dZ.shape == Z.shape)
+
+    return dZ
