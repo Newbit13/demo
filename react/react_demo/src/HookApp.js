65f455e9d43c0557be04a3bc3fc50eda319066cb
Newbit13 <525842854@qq.com>
2021-12-15 10:49:04 +0800

    自定义hooks

--- react/react_demo/src/HookApp.js
+++ react/react_demo/src/HookApp.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from "react";
 // import { connect } from 'dva';
 
 // function IndexPage(props) {
@@ -25,32 +25,50 @@ import React, { useCallback } from 'react';
 //   return state.example;
 // })(IndexPage);
 
-import { useSelector,useDispatch } from 'dva';
+import { useSelector, useDispatch } from "dva";
+import { useState } from "react";
 // import {  useSelector } from 'react-redux';
+function useTime() {
+  const [time, setT] = useState(0);
+  useEffect(() => {
+    let t = setInterval(() => {
+      setT(time + 1);
+    }, 1000);
+    return () => {
+      clearInterval(t);
+    };
+  }, [time]);
+  return time;
+}
 function IndexPage(props) {
+  const time = useTime();
   const dispatch = useDispatch();
-  const {data1,data2} = useSelector(data=>{
+  const { data1, data2 } = useSelector((data) => {
     return {
-      data1:data.example,
-      data2:data.example2,
-    }
+      data1: data.example,
+      data2: data.example2,
+    };
   });
   const v = data1.a.value;
-  const handleLog = useCallback((id)=>{
-    console.log(id);
-    console.log(v);
-  },[data1]);
+  const handleLog = useCallback(
+    (id) => {
+      console.log(id);
+      console.log(v);
+    },
+    [data1]
+  );
   console.log(v);
-  const handleAdd = useCallback(()=>{
+  const handleAdd = useCallback(() => {
     dispatch({
-      type:'example/add'
-    })
-  },[]);
+      type: "example/add",
+    });
+  }, []);
   return (
     <div>
       <button onClick={handleAdd}>click</button>
-      <button onClick={handleLog.bind(this,777)}>log</button>
+      <button onClick={handleLog.bind(this, 777)}>log</button>
       <div>{data1.a.value}</div>
+      <div>{time}</div>
     </div>
   );
 }
--- react/react_demo/src/index.js
+++ react/react_demo/src/index.js
@@ -1,5 +1,5 @@
-import dva from 'dva';
-
+import dva from "dva";
+import router from "./router";
 // 1. Initialize
 const app = dva();
 
@@ -8,21 +8,23 @@ const app = dva();
 
 // 3. Model
 app.model({
-  namespace: 'example',
+  namespace: "example",
   state: {
-    a:{
-      value:123
-    }
+    a: {
+      value: 123,
+    },
   },
 
   subscriptions: {
-    setup({ dispatch, history }) {  // eslint-disable-line
+    setup({ dispatch, history }) {
+      // eslint-disable-line
     },
   },
 
   effects: {
-    *fetch({ payload }, { call, put }) {  // eslint-disable-line
-      yield put({ type: 'save' });
+    *fetch({ payload }, { call, put }) {
+      // eslint-disable-line
+      yield put({ type: "save" });
     },
   },
 
@@ -31,33 +33,34 @@ app.model({
       return { ...state, ...action.payload };
     },
     add(state, action) {
-      return { 
-        ...state, 
-        a:{
-          value:++state.a.value
-        }
+      return {
+        ...state,
+        a: {
+          value: ++state.a.value,
+        },
       };
     },
   },
-
 });
 
 app.model({
-  namespace: 'example2',
+  namespace: "example2",
   state: {
-    a:{
-      value:123
-    }
+    a: {
+      value: 123,
+    },
   },
 
   subscriptions: {
-    setup({ dispatch, history }) {  // eslint-disable-line
+    setup({ dispatch, history }) {
+      // eslint-disable-line
     },
   },
 
   effects: {
-    *fetch({ payload }, { call, put }) {  // eslint-disable-line
-      yield put({ type: 'save' });
+    *fetch({ payload }, { call, put }) {
+      // eslint-disable-line
+      yield put({ type: "save" });
     },
   },
 
@@ -66,20 +69,18 @@ app.model({
       return { ...state, ...action.payload };
     },
     add(state, action) {
-      return { 
-        ...state, 
-        a:{
-          value:++state.a.value
-        }
+      return {
+        ...state,
+        a: {
+          value: ++state.a.value,
+        },
       };
     },
   },
-
 });
 
 // 4. Router
-app.router(require('./router').default);
+app.router(router);
 
 // 5. Start
-app.start('#root');
-
+app.start("#root");
