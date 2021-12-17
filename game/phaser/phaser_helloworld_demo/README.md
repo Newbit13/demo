28342e407ef072f0cff5ed334b8c223c52a8fdc2
Newbit13 <525842854@qq.com>
2021-12-17 17:19:16 +0800

    feat：phaser hello world

---
+++ game/phaser/phaser_helloworld_demo/README.md
@@ -0,0 +1,3 @@
+npm init vite@latest
+
+npm install phaser@3.55.2
---
+++ game/phaser/phaser_helloworld_demo/main.js
@@ -0,0 +1,55 @@
+import "./style.css";
+import Phaser from "phaser";
+
+let widW = window.innerWidth;
+let widH = window.innerHeight;
+
+var config = {
+  type: Phaser.AUTO,
+  width: widW,
+  height: widH - 4,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
+  scene: {
+    preload: preload,
+    create: create,
+  },
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+  // this.load.setBaseURL("http://labs.phaser.io");
+
+  // this.load.image("sky", "assets/skies/space3.png");
+  // this.load.image("logo", "assets/sprites/phaser3-logo.png");
+  // this.load.image("red", "assets/particles/red.png");
+
+  this.load.image("sky", "assets/space3.png");
+  this.load.image("logo", "assets/phaser3-logo.png");
+  this.load.image("red", "assets/red.png");
+}
+
+function create() {
+  this.add.image(400, 300, "sky");
+
+  var particles = this.add.particles("red");
+
+  var emitter = particles.createEmitter({
+    speed: 100,
+    scale: { start: 1, end: 0 },
+    blendMode: "ADD",
+  });
+
+  var logo = this.physics.add.image(400, 100, "logo");
+
+  logo.setVelocity(100, 200);
+  logo.setBounce(1, 1);
+  logo.setCollideWorldBounds(true);
+
+  emitter.startFollow(logo);
+}
