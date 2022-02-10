71f1f6d3cec72a025aadf99fb0e496a48c54b9d5
黄骏泳 <525842854@qq.com>
2022-02-10 19:12:18 +0800

    feat: 冰墩墩ing

---
+++ threejs_demo/2022-bingdundun/src/index.js
@@ -0,0 +1,52 @@
+import * as THREE from 'three'
+import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { TWEEN } from "three/examples/jsm/libs/tween.module.min.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// import bingdundunModel from './models/bingdundun.glb';
+
+function init() {
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);//renderer.domElement 一个canvas
+    // 场景
+    const scene = new THREE.Scene();
+    // 相机
+    const camera = new THREE.PerspectiveCamera(
+        90,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        100
+    );
+    camera.position.set(10, 0, 0);
+
+    // 轨道控制器
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.addEventListener("change", render);
+    controls.minDistance = 1;
+    controls.maxDistance = 200;
+    controls.enablePan = false;
+
+    // 新增一个红色球
+    const geometry = new THREE.SphereGeometry(1, 10, 10);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+    // 坐标轴辅助线
+    scene.add(new THREE.AxisHelper(1000));
+
+    controls.update(); // 控制器需要
+    controls.target.copy(mesh.position);
+
+    function render() {
+        renderer.render(scene, camera);
+    }
+
+    function r() {
+        render();
+        requestAnimationFrame(r)
+    }
+    r()
+}
+
+init();
---
+++ threejs_demo/2022-bingdundun/webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+const webpackConfig = {
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env',
+                                    {
+                                        modules: "commonjs"
+                                    }
+                                ]
+                            ],
+                            // plugins: [['@babel/plugin-transform-runtime', { corejs: 3 }]]
+                        }
+                    }
+                ]
+            },{
+                test:/\.(glb|stl)$/,
+                loader:'file-loader',
+            }
+        ]
+    },
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+    ],
+}
+
+module.exports = webpackConfig;
